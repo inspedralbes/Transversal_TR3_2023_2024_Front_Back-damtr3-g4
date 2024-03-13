2d898ba0307d0de092f974f358a94238e06cd714
AndresOreVel <121023104+AndresOreVel@users.noreply.github.com>
2024-03-13 10:49:19 +0100

    comit en front

--- front/pages/loggin.vue
+++ front/pages/loggin.vue
@@ -1,45 +1,73 @@
 <template>
-    <div class="contenedor">
-      <h1>Ingresa tus datos</h1>
-      <form @submit.prevent="submitForm">
-        <div>
-          <label for="username">Username: </label>
-          <input type="text" id="username" v-model="username">
-        </div>
-        <div>
-          <label for="password">Password: </label>
-          <input type="text" id="password" v-model="password">
-        </div>
-        <button type="submit">Iniciar sesión</button>
-      </form>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  import { useRouter } from 'vue-router';
-  
-  const username = ref('');
-  const password = ref('');
-  const router = useRouter();
-  
-  const submitForm = () => {
-    // Aquí puedes realizar acciones como enviar los datos al servidor, etc.
-    // Por ahora, simplemente redirigiremos a otra página.
-    router.push('/');
-  };
-  </script>
+  <div class="contenedor">
+    <v-card class="fondo">
+      <div class="datos">
+        <h1>Ingresa tus datos</h1>
+        <form @submit.prevent="submitForm" class="datos">
+          <div class="campo">
+            <label for="username">Username: </label>
+            <input class="input" type="text" id="username" v-model="username">
+          </div>
+          <div class="campo">
+            <label for="password">Password: </label>
+            <input class="input" type="password" id="password" v-model="password">
+          </div>
+          <div class="campo">
+            <button class="input" type="submit">Iniciar sesión</button>
+          </div>
+        </form>
+      </div>
+    </v-card>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
-  <style>
-  .contenedor{
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    background-image: url('../juego.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-  }
+const username = ref('');
+const password = ref('');
+const router = useRouter();
+
+const submitForm = () => {
+  // Aquí puedes realizar acciones como enviar los datos al servidor, etc.
+  // Por ahora, simplemente redirigiremos a otra página.
+  router.push('/');
+};
+</script>
+
+<style>
+.datos {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+}
+
+.contenedor {
+  display: flex;
+  flex-direction: row-reverse;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url('../juego.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+.fondo {
+  width: 400px;
+  height: 200px;
+  border-radius: 10px;
+  background-color: aquamarine;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1); /* Sombra suave */
+}
+
+.input {
+  border-radius: 50px;
+}
+.campo{
+  margin-bottom: 10px;
+}
 </style>
