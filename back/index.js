8308ec97f27d8d7097779d7bb3a5e05130b157b6
a22jhepincre <a22jhepincre@inspedralbes.cat>
2024-03-13 13:04:01 +0100

    sockets in progress

--- back/index.js
+++ back/index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const fs = require("fs");
 const cors = require("cors");
 const CryptoJS = require("crypto-js");
+const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
@@ -11,6 +12,13 @@ const port = 3789;
 
 app.use(express.json());
 app.use(cors());
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+    },
+});
 
 const {
     selectUserByMailPass,
@@ -38,9 +46,39 @@ app.post("/authoritzationLogin", async (req, res) => {
 app.post("/insertUser", async (req, res) => {
     const user = req.body;
     user.password = doCryptMD5Hash(req.body.password);
-    await insertUser(user.name,  user.password, user.mail);
+    await insertUser(user.name, user.password, user.mail);
     res.send({ response: "User inserted correctly", userData: user });
 });
+
+// SOCKETS
+
+var usersConnected = [];
+
+io.on("connection", async (socket) => {
+    socket.on("login", async () => {
+        console.log("DENTRO DE LOGIN PAGE::    ");
+        usersConnected.push({ socketId: socket, userId: id })
+        usersConnected.forEach(u => {
+            console.log("USER ID:: ", u.userId);
+            console.log("USER SOCKET:: ", u.socketId.id);
+        });
+    });
+
+    socket.on("actualizarObjeto", ()=>{
+        
+    });
+
+    socket.on("disconnect", () => {
+        usersConnected.forEach(u => {
+            if (u.socketId == socket) {
+                usersConnected.pop(u)
+            }
+            console.log("USER ID:: ", u.userId);
+            console.log("USER SOCKET:: ", u.socketId.id);
+        });
+    });
+});
+
 
 server.listen(port, () => {
     console.log(`Server running on http//:localhost:${port}`);
