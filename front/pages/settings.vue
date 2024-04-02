bef9003e2d1ea830347a37ccbd9fb2b8355150a7
Deraybcbc <a22kevburcac@inspedralbes.cat>
2024-04-02 12:28:38 +0200

    dsg

--- front/pages/settings.vue
+++ front/pages/settings.vue
@@ -19,32 +19,52 @@
             </div>
         </div>
         <div class="container-audio">
-    <!-- Sección de menú -->
-    <div class="container-menu">
-        <h3 style="font-family: gaming;">Menú</h3>
-        <div v-for="(menuAudio, index) in menuAudios" :key="index" class="audio-item">
-            <input type="radio" v-model="selectedMenuAudio" :value="menuAudio" @change="selectMenuAudio(menuAudio)">
-            <audio :src="menuAudio" type="audio/mpeg" controls>
-                Tu navegador no soporta el elemento de audio.
-            </audio>
+            <!-- Sección de menú -->
+            <div class="container-menu">
+                <h3 style="font-family: gaming; display: flex; justify-content: center;">Menú</h3>
+                <div v-for="(menuAudio, index) in menuAudios" :key="index" class="audio-item">
+                    <input type="radio" v-model="selectedMenuAudio" :value="menuAudio"
+                        @change="selectMenuAudio(menuAudio)">
+                    <audio :src="menuAudio" type="audio/mpeg" controls>
+                        Tu navegador no soporta el elemento de audio.
+                    </audio>
+                </div>
+            </div>
+            <!-- Sección de batalla -->
+            <div class="container-battle">
+                <h3 style="font-family: gaming; display: flex; justify-content: center;">Batalla</h3>
+                <div v-for="(battleAudio, index) in battleAudios" :key="index" class="audio-item">
+                    <input type="radio" v-model="selectedBattleAudio" :value="battleAudio"
+                        @change="selectBattleAudio(battleAudio)">
+                    <audio :src="battleAudio" type="audio/mpeg" controls>
+                        Tu navegador no soporta el elemento de audio.
+                    </audio>
+                </div>
+            </div>
         </div>
-    </div>
-    <!-- Sección de batalla -->
-    <div class="container-battle">
-        <h3 style="font-family: gaming;">Batalla</h3>
-        <div v-for="(battleAudio, index) in battleAudios" :key="index" class="audio-item">
-            <input type="radio" v-model="selectedBattleAudio" :value="battleAudio" @change="selectBattleAudio(battleAudio)">
-            <audio :src="battleAudio" type="audio/mpeg" controls>
-                Tu navegador no soporta el elemento de audio.
-            </audio>
+        <div class="container-odoo">
+            <div class="buttons-odoo">
+                <div class="button_pair">
+                </div>
+                <div class="button_pair">
+                    <div class="btn">
+                        <button class="button4" @click="statusOdoo(true)">
+                            <span class="button_text">PLAY</span>
+                        </button>
+                    </div>
+                    <div class="btn">
+                        <button class="button3" @click="statusOdoo(false)">
+                            <span class="button_text">STOP</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
     </main>
 </template>
 <script>
 import Navigation from '~/layouts/Navigation.vue';
-import { getBroadcast, getAudios, sendAudio } from '~/services/communicationManager';
+import { getBroadcast, getAudios, sendAudio, procesOdoo } from '~/services/communicationManager';
 export default {
     components: {
         Navigation,
@@ -57,6 +77,7 @@ export default {
             battleAudios: [], // Array para audios de batalla
             selectedMenuAudio: [], // Audio del menú seleccionado
             selectedBattleAudio: [], // Audio de batalla seleccionado
+            isActive: false
         }
     },
     async created() {
@@ -82,12 +103,24 @@ export default {
             console.log(audio);
             const res = await sendAudio(audio);
             console.log(res);
+        },
+        async statusOdoo(isActive){
+            const proces = await procesOdoo(isActive);
+            console.log(isActive);
         }
     }
 
 }
 </script>
 <style>
+main {
+    display: flex;
+    flex-direction: column;
+    /* Cambiar la dirección del contenedor principal a columna */
+    align-items: center;
+    /* Alinear los elementos horizontalmente en el centro */
+}
+
 .container-broadcast {
     display: flex;
     flex-wrap: wrap;
@@ -95,7 +128,8 @@ export default {
     padding: 10px;
     border: 2px black solid;
     position: relative;
-    margin-top: 30px;
+    margin: 30px;
+    width: 80%;
 }
 
 .container-broadcast:before {
@@ -117,6 +151,7 @@ export default {
     margin-top: 10px;
     border: 2px black solid;
     position: relative;
+    width: 80%;
 }
 
 .container-audio:before {
@@ -140,9 +175,7 @@ export default {
 
 .cardSettings {
     width: 250px;
-    /* Puedes ajustar según tu diseño */
     height: 220px;
-    /* Puedes ajustar según tu diseño */
     background: rgba(217, 217, 217, 0.58);
     box-shadow: 12px 17px 51px rgba(0, 0, 0, 0.22);
     backdrop-filter: blur(6px);
@@ -211,4 +244,99 @@ export default {
     color: hsl(0, 0%, 100%);
     box-shadow: rgba(0, 0, 0, 0.8) 0px 7px 29px 0px;
 }
+
+.container-odoo {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    padding: 10px;
+    margin-top: 10px;
+    border: 2px black solid;
+    position: relative;
+    width: 80%;
+}
+
+.container-odoo:before {
+    content: "Odoo proces";
+    position: absolute;
+    font-family: gaming;
+    top: -10px;
+    left: 8%;
+    transform: translateX(-50%);
+    background-color: #ffffff;
+    padding: 0 10px;
+}
+
+.buttons-odoo {
+  display: flex;
+  flex-direction: column;
+  row-gap: 1.5em;
+}
+.button_pair {
+  display: flex;
+  column-gap: 1.5em;
+}
+.button_pair1 {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.9em;
+}
+
+.button3 {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 5.7em;
+  height: 5.7em;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  background-color: #d42a02;
+  box-shadow: rgba(0, 0, 0, 0.377) 10px 10px 8px, #fb702c 2px 2px 10px 0px inset,
+    #d42a02 -4px -4px 1px 0px inset;
+  cursor: pointer;
+  font-family: Montserrat;
+  transition: 0.1s ease-in-out;
+}
+.button4 {
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  width: 5.7em;
+  height: 5.7em;
+  border-radius: 10px;
+  border: none;
+  outline: none;
+  background-color: #545251;
+  box-shadow: rgba(0, 0, 0, 0.377) 10px 10px 8px,
+    #a8a6a4 1.5px 1.5px 1px 0px inset, #545251 -3.2px -3.2px 8px 0px inset;
+  cursor: pointer;
+  font-family: Montserrat;
+  transition: 0.1s ease-in-out;
+}
+
+.button_text {
+  color: white;
+  font-family: gaming;
+  padding-top: 0.9em;
+  letter-spacing: 0.075em;
+  font-size: 0.85em;
+  transition: 0.1s ease-in-out;
+}
+
+.button3:active {
+  box-shadow: rgba(0, 0, 0, 0.377) 0px 0px 0px, inset 0.5px 0.5px 4px #000000,
+    #d42a02 -3.2px -3.2px 8px 0px inset;
+}
+.button3:active .button_text {
+  transform: translateY(0.5px);
+}
+.button4:active {
+  box-shadow: rgba(0, 0, 0, 0.377) 0px 0px 0px, inset 0.5px 0.5px 4px #000000,
+    #545251 -3.2px -3.2px 8px 0px inset;
+}
+.button4:active .button_text {
+  transform: translateY(0.5px);
+}
+
 </style>
